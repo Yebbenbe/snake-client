154b8f61f694c9cbd83839f2c416ac49520c9874
Yebbenbe <lmtpox@gmail.com>
2023-10-17 02:28:04 -0300

    implemented send name on connect, proper file :)

---
+++ client.js
@@ -0,0 +1,30 @@
+// client.js
+const net = require("net");
+
+// establishes a connection with the game server
+const connect = function () {
+  const conn = net.createConnection({
+    host: "localhost",
+    port: 50541,
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+  // listen for data, log what comes
+  conn.on("data", (data) => {
+    console.log("Server says:", data); // Log incoming data from the server
+  });
+
+  conn.on("connect", () => {
+    console.log("Successfully connected to game server");
+  });
+
+  conn.on("connect", () => {
+    conn.write("Name: BJJ");
+  });  
+
+  return conn;
+};
+
+module.exports = {connect};
