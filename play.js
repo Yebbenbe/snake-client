1bf20a2413f11354671118119f1e9ee3253e28d9
Yebbenbe <lmtpox@gmail.com>
2023-10-20 07:46:05 -0300

    add listener for stdin

--- play.js
+++ play.js
@@ -3,4 +3,23 @@ const { connect } = require("./client");
 
 console.log("Connecting to server...");
 
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  // event listener, makes a callback to handleUserInput
+  stdin.on("data", handleUserInput);
+  return stdin;
+};
+
+const handleUserInput = function () {
+  process.stdin.on('data', (key) => {
+    // check for ctrl+c input
+    if (key === '\u0003') {
+      process.exit();
+    }
+  })
+}
+setupInput();
 connect();
